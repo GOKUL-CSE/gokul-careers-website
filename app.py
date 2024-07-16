0af81de04d3662b49ed90df80cb596209a32f100
GOKUL-CSE <94049671+GOKUL-CSE@users.noreply.github.com>
2024-07-16 16:58:32 +0000

    templates added

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
 from re import DEBUG, template
 from flask import Flask, render_template
 app = Flask(__name__)
+JOBS = [
+    {
+        'id' : 1,
+        'role' : "Data Analyst",
+        'salary' : "Rs. 10,00,000"
+    },
+    {
+        'id' : 2,
+        'role' : "Data Scientist",
+        'salary' : "Rs. 20,00,000"
+    },
+    {
+        'id' : 3,
+        'role' : "Machine Learning Engineer",
+        'salary': "Rs. 15,00,000"
+    },
+    {
+        'id' : 4,
+        'role' : "Data Engineer",
+        'salary' : "Rs. 10,00,000"
+    }
+
+    
+]
+
+
+
+
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',jobs = JOBS)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug  = True)
